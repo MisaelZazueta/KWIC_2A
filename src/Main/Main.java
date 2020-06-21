7f31ca66b7d0fd8f7d1302b2926ae1d3e22a60a4
Misael <ingmisaelzazueta@gmail.com>
2020-06-21 17:20:51 -0600

    Comentarios explicativos de los directorios del sistema.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,3 +1,16 @@
+/*Sistema KWIC v2.1
+* Autor: Ing. Jose Misael Burruel Zazueta
+*
+* En la carpeta SRC se encuentran los directorios que componen al sistema KWIC v2.1
+* Estos directorios se nombran segun su proposito en el sistema:
+* Main: Se ecuentra la clase main, que es la aplicacion del sistema.
+* KWIC: Se encuentran los directorios del KWIC v1, que son utilizados para la v2.1
+* FullIndexGenerator: Se ecuentran las clases que se encargan de generar el index completo de palabras
+* PhraseSearcher: Se encuentran las clases que se encargan de generar el indice de frases a buscar
+* WordSearcher: Se encuentran las clases que se enccargan de generar el indice de palabras especificas a buscar
+* DocSeacher: Se encuentran las clases que se encargan de buscar un archivo en un directorio
+* AndSeacher: Se ecuentran las clases que se encargan de buscar palabras aleatorias (no frases) en una misma pagina
+*/
 package Main;
 
 import AndSearcher.And;
